167187dcbefae5bf5746465a5dc10f4583238932
Abinesh <abinesh@xcelcorp.com>
2022-09-23 18:40:13 +0530

    page1-design

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -4,21 +4,28 @@ import "../styles/page1.css";
 function Page1() {
   return (
     <>
-      <div className="center">
-        <div className="design">
-          <div className="des">
-            <p>D E S</p>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="center">
+          <div className="design">
+            <div className="des">
+              <p>D E S</p>
+            </div>
+          </div>
+          <div className="text">
+            <div className="toptext">Interaction Design Work</div>
+            <hr></hr>
+            <div className="bottomtext">Graphic Designs WORK</div>
+          </div>
+          <div className="ign">
+            <p>
+              I<span id="space"></span> G N
+            </p>
           </div>
         </div>
-        <div className="text">
-          <div className="toptext">Interaction Design Work</div>
-          <hr></hr>
-          <div className="bottomtext">Graphic Designs WORK</div>
-        </div>
-        <div className="ign">
-          <p>
-            I<span id="space"></span> G N
-          </p>
+        <div>
+          <div>
+            <h1>HELLO,</h1>
+          </div>
         </div>
       </div>
     </>
