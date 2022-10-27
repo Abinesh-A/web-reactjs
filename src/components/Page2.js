60ff127855b17c48faa104ca4006ea1e23dfd401
Abinesh <abinesh@xcelcorp.com>
2022-10-27 12:46:56 +0530

    about ui...

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -4,8 +4,18 @@ export const Page2 = ({ about }) => {
   return (
     <div className="page2div" ref={about}>
       <div className="aboutdiv">
-        <div className="about_h">
+        {/* <div className="about_h">
           ABOUT ME
+        </div> */}
+        <div class="about_h">
+          <h1>
+            ABOUT ME<span>HELLO I AM ABINESH</span>
+          </h1>
+          <div class="container">
+            <div class="chevron"></div>
+            <div class="chevron"></div>
+            <div class="chevron"></div>
+          </div>
         </div>
         <div className="about_t">
           In my younger and more vulnerable years my father gave me some advice
